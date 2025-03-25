b0313ff2601e1b0ee63bfcb3d3c2694d90041b6d
AndyBS1 <andy230505@gmail.com>
2025-03-25 08:32:08 -0600

    cambios en el formato del mensaje

--- enviarmensaje.cpp
+++ enviarmensaje.cpp
@@ -5,9 +5,14 @@
 #include <unistd.h>
 
 int main() {
-    std::string usuario;
-    std::cout << "Nombre de usuario: ";
-    std::cin >> usuario;
+    std::string usuario_origen;
+    std::cout << "De: ";
+    std::cin >> usuario_origen;
+    std::cin.ignore();
+
+    std::string usuario_destino;
+    std::cout << "Para: ";
+    std::cin >> usuario_destino;
     std::cin.ignore(); // Se limpia el buffer
     
     std::string mensaje;
@@ -26,10 +31,14 @@ int main() {
         return 1;
     }
     
-    std::string mensaje_formato = "MENSAJE|" + usuario + "|" + mensaje;
+    // Formato del mensaje
+    std::string mensaje_formato = "MENSAJE|" + usuario_origen + "|" + usuario_destino + "|" + mensaje;
+    
+    // Envio del mensaje al server
     send(sock, mensaje_formato.c_str(), mensaje_formato.size(), 0);
     
     std::cout << "Mensaje enviado al servidor.\n";
+    
     close(sock);
     return 0;
 }
